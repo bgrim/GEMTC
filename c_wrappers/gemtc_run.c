e1e280a0ba296e30e18521f461e24b379315ac94
Scott Krieder <skrieder@iit.edu>
2012-11-01 14:08:12 -0500

    split into c_wrapper files

--- c_wrappers/gemtc_run.c
+++ c_wrappers/gemtc_run.c
@@ -1,20 +1,13 @@
-//extern void setupGemtc(int);
 extern void *run(int, int, void*, int);
-//extern void cleanupGemtc(void);
 
 #include<stdio.h>
 
 int main(int argc, char **argv){
-  // declare the size to be shared by both incoming and outgoing queues on the device
-  //  setupGemtc(2560);
-
   int i;
   for(i=0; i<5; i++){
     int sleepTime = 1000;
     void *ret = run(0, 32, &sleepTime, sizeof(int));
     printf("Finished job with parameter: %d\n", *(int *)ret);
   }
-  //cleanupGemtc();
-
   return 0;
 }
--- c_wrappers/gemtc_setup.c
+++ c_wrappers/gemtc_setup.c
@@ -1,19 +1,8 @@
 extern void setupGemtc(int);
-//extern void *run(int, int, void*, int);
-//extern void cleanupGemtc(void);
 
 #include<stdio.h>
 
 int main(int argc, char **argv){
   setupGemtc(2560);
-
-  /*int i;
-  for(i=0; i<5; i++){
-    int sleepTime = 1000;
-    void *ret = run(0, 32, &sleepTime, sizeof(int));
-    printf("Finished job with parameter: %d\n", *(int *)ret);
-  }
-  cleanupGemtc();
-  */
   return 0;
 }
