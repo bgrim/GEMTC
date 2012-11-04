a843463d0454f5442a781a7e09207fd402ea7c96
Scott Krieder <skrieder@iit.edu>
2012-11-04 15:40:49 -0600

    Duplicated the foo example with a bar example for further demonstration.

---
+++ c_wrappers/test-bar/bar.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+ 
+ 
+void bar(void)
+{
+  puts("Hello, I'm a shared library, and I'm printing from bar.");
+}
---
+++ c_wrappers/test-bar/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+ 
+int main(void)
+{
+  puts("This is a shared library test...");
+  bar();
+  return 0;
+}
