7f43bf79fb6be847f8052ccdb94eace5f6082f80
Scott Krieder <skrieder@iit.edu>
2012-11-05 10:29:24 -0600

    Code is working and compiling from a c file using .so rather than includes

--- c_wrappers/test-setup/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "main.h"
- 
-int main(void)
-{
-  puts("This is a shared library test...");
-  bar();
-  return 0;
-}
--- c_wrappers/test-setup/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef bar_h__
-#define bar_h__
- 
-extern void bar(void);
- 
-#endif  // bar_h__
